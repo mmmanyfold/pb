eb4aa05d26b88e7265bac261dad306c9d1f58a67
David A. Viramontes <dviramontes@gmail.com>
2018-04-16 20:35:05 -0600

    refactor: voters table

--- resources/migrations/20180412192547-add-voters-table.up.sql
+++ resources/migrations/20180412192547-add-voters-table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE voters
 (id VARCHAR(20) PRIMARY KEY,
  first_name VARCHAR(30),
  last_name VARCHAR(30),
- email VARCHAR(30),
+ phone VARCHAR(10),
  admin BOOLEAN,
  last_login TIMESTAMP,
  is_active BOOLEAN,
