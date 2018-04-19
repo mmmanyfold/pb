d93d993cf7f4cf92d027ac183f1a81538606925c
eemshi <oreneemshi@gmail.com>
2018-04-18 22:42:33 -0600

    add selected proposal state & styles, remove button

--- src/cljs/pb/components/proposal.cljs
+++ src/cljs/pb/components/proposal.cljs
@@ -2,28 +2,40 @@
   (:require [reagent.core :as rg]))
 
 (defn proposal-component []
-  (let [show?a (rg/atom false)
-        show?b (rg/atom false)
-        show?c (rg/atom false)]
+  (let [show-impact? (rg/atom false)
+        show-budget? (rg/atom false)
+        show-timeline? (rg/atom false)
+        selected? (rg/atom false)]
     (fn []
       [:div.proposal-component.pa4.col-xs-12.col-md-6.col-lg-4
+       {:style {:background-color (if @selected? "rgba(115,159,62,0.15)" "white")}}
+
        [:h2.fw7 "Walking Art Gallery"]
        [:p.f4 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]
-       [:h5 {:on-click #(swap! show?a not)
-             :class (when @show?a "b")}
-        "+ Community Impact & Supporting Statistics"]
-       [:div {:class (if @show?a "expand" "hide")}
-        [:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]]
-       [:h5 {:on-click #(swap! show?b not)
-             :class (when @show?b "b")}
-        "+ Budget Breakdown"]
-       [:div {:class (if @show?b "expand" "hide")}
-        [:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]]
-       [:h5 {:on-click #(swap! show?c not)
-             :class (when @show?c "b")}
-        "+ Timeline"]
-       [:div {:class (if @show?c "expand" "hide")}
-        [:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]]
-       [:div.tc
-        [:button.select.mt2.mb3 "Select"]]
+       [:div.tc.mt2.mb3
+          [:button.mv2 {:on-click #(swap! selected? not)
+                        :class (if @selected? "selected" "select")}
+             (if @selected? "Selected" "Select")]
+          (when @selected?
+             [:button.remove.mv2.ml3 {:on-click #(swap! selected? not)}
+                "Remove"])]
+
+       [:h5 {:on-click #(swap! show-impact? not)
+             :class (when @show-impact? "b")}
+          "+ Community Impact & Supporting Statistics"]
+       (when @show-impact?
+          [:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."])
+
+       [:h5 {:on-click #(swap! show-budget? not)
+             :class (when @show-budget? "b")}
+          "+ Budget Breakdown"]
+       (when @show-budget?
+          [:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."])
+
+       [:h5 {:on-click #(swap! show-timeline? not)
+             :class (when @show-timeline? "b")}
+          "+ Timeline"]
+       (when @show-timeline?
+          [:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."])
+
        [:img.w-100.mt2 {:src "img/temp.jpg"}]])))
