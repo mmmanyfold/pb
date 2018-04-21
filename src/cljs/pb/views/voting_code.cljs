ee55861bdae2bc2ca005861064cd61d1bdb32a93
eemshi <oreneemshi@gmail.com>
2018-04-20 22:02:29 -0600

    handle voting-code-view if online voting hasn't started

--- src/cljs/pb/views/voting_code.cljs
+++ src/cljs/pb/views/voting_code.cljs
@@ -10,7 +10,6 @@
         query (str "{ elections(q: \"fields.shortTitle=" election-slug
                    "\") {
                      title
-                     shortTitle
                      startOnline
                      endOnline
                      proposals {
@@ -18,27 +17,35 @@
                    }}")]
     (rf/dispatch [:get-contentful-data db-key query :election])
     (fn [election-slug]
-      (when-let [{:keys [endOnline]} @(rf/subscribe [:election-in-view])]
+      (when-let [{:keys [startOnline endOnline]} @(rf/subscribe [:election-in-view])]
         (if (> (js/Date. endOnline) (js/Date.))
+          (if (> (js/Date. startOnline) (js/Date.))
+            ;; if online voting hasn't started
+            [:div.voting-code-view
+             [:h1 (str "Voting opens on "
+                       (.format (js/moment startOnline) "dddd, MMMM Do YYYY [at] h:mm a."))]]
+            ;; if online voting is open
+            [:div.voting-code-view
+             [:h1 "Text " [:span#text-keyword election-slug] " to 1234567890 to get your voting code"]
+             [:p "Your phone number will NEVER be shared and will automatically get deleted after the election."
+              [:br]
+              "Your voting code is specific to this election and won’t be used for anything else."]
+             [:div.input-group.flexrow-wrap
+              [:div.input-group-prepend
+               [:input.form-control
+                {:type "number"
+                 :placeholder "000000"
+                 :min 1
+                 :max 999999
+                 :value @code
+                 :on-change (fn [e]
+                              (let [input (-> e .-target .-value)]
+                                (reset! code input)))}]]
+              [:a {:href (str "/#/" election-slug "/proposals")}
+               [:input
+                {:type "submit"
+                 :value "VOTE"}]]]])
+          ;; if online voting has ended
           [:div.voting-code-view
-           [:h1 "Text " [:span#text-keyword election-slug] " to 1234567890 to get your voting code"]
-           [:p "Your phone number will NEVER be shared and will automatically get deleted after the election."
-            [:br]
-            "Your voting code is specific to this election and won’t be used for anything else."]
-           [:div.input-group.flexrow-wrap
-            [:div.input-group-prepend
-             [:input.form-control
-              {:type "number"
-               :placeholder "000000"
-               :min 1
-               :max 999999
-               :value @code
-               :on-change (fn [e]
-                            (let [input (-> e .-target .-value)]
-                              (reset! code input)))}]]
-            [:a {:href (str "/#/" election-slug "/proposals")}
-             [:input
-              {:type "submit"
-               :value "VOTE"}]]]]
-          [:div.voting-code-view
-           [:h1 (str "Voting ended on " (.format (js/moment endOnline) "dddd, MMMM Do YYYY [at] h:mm a."))]])))))
+           [:h1 (str "Voting ended on "
+                     (.format (js/moment endOnline) "dddd, MMMM Do YYYY [at] h:mm a."))]])))))
