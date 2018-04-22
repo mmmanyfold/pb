0d29d02e2fcb5b63ffcf15e60726d396c103c25f
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-04-22 15:16:54 -0600

    Logic for creating new voter or returning the code of existing voter(broken)

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,8 +1,8 @@
 -- :name create-voter! :! :n
 -- :doc creates a new voter record
 INSERT INTO voters
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+(phone, admin, is_active, code)
+VALUES (:phone, :admin, :is_active, :code)
 
 -- :name update-voter! :! :n
 -- :doc updates an existing voter record
@@ -10,10 +10,14 @@ UPDATE voters
 SET first_name = :first_name, last_name = :last_name, email = :email
 WHERE id = :id
 
+-- :name all-voters :? :*
+-- :doc foo
+SELECT * FROM voters
+
 -- :name get-voter :? :1
 -- :doc retrieves a voter record given the id
 SELECT * FROM voters
-WHERE id = :id
+WHERE phone = :phone
 
 -- :name delete-voter! :! :n
 -- :doc deletes a voter record given the id
