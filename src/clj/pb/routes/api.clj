c3ddca4891cd21aba17996845e4434640e07b563
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-04-24 14:21:18 -0600

    Adds let binding for 8 digit code from full hash

--- src/clj/pb/routes/api.clj
+++ src/clj/pb/routes/api.clj
@@ -5,7 +5,8 @@
             [ring.util.http-response :as response]
             [clojure.java.io :as io]
             [buddy.hashers :as hashers]
-            [clojure.java.jdbc :as jdbc]))
+            [clojure.java.jdbc :as jdbc]
+            [clojure.string :as str]))
 
 (defn db-tx [f & [args]]
   (jdbc/with-db-transaction [t-conn *db*]
@@ -17,9 +18,11 @@
   [req]
   (let [phone-number (get-in req [:params :phone-number])]
     (if-let [voter (db-tx db/get-voter {:phone phone-number})]
-      (let [code (:code voter)])
+      (let [code (:code voter)
+            voting-code (subs (str/replace (str/replace code "pbkdf2+sha3_256" "") "$" "") 0 8)])
         ;return pre-existing code via twilio)
-      (let [code (hashers/derive phone-number {:alg :pbkdf2+sha3_256})]
+      (let [code (hashers/derive phone-number {:alg :pbkdf2+sha3_256})
+            voting-code (subs (str/replace (str/replace code "pbkdf2+sha3_256" "") "$" "") 0 8)]
         (db-tx db/create-voter! {:phone phone-number
                                  :admin false
                                  :is_active true
