fce8cbecfba09029ecc3bdd90b113b866c090278
eemshi <oreneemshi@gmail.com>
2018-04-24 15:41:12 -0600

    add twilio dep, environment variables, & send-sms fn calls

--- src/clj/pb/routes/api.clj
+++ src/clj/pb/routes/api.clj
@@ -6,7 +6,12 @@
             [clojure.java.io :as io]
             [buddy.hashers :as hashers]
             [clojure.java.jdbc :as jdbc]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [twilio.core :as twilio]))
+
+(defonce PB_TWILIO_AUTH_TOKEN (System/getenv "PB_TWILIO_AUTH_TOKEN"))
+(defonce PB_TWILIO_SERVICE_SID (System/getenv "PB_TWILIO_SERVICE_SID"))
+(defonce PB_TWILIO_PHONE_NUMBER (System/getenv "PB_TWILIO_PHONE_NUMBER"))
 
 (defn db-tx [f & [args]]
   (jdbc/with-db-transaction [t-conn *db*]
@@ -19,15 +24,23 @@
   (let [phone-number (get-in req [:params :phone-number])]
     (if-let [voter (db-tx db/get-voter {:phone phone-number})]
       (let [code (:code voter)
-            voting-code (subs (str/replace (str/replace code "pbkdf2+sha3_256" "") "$" "") 0 8)])
-        ;return pre-existing code via twilio)
+            voting-code (subs (str/replace (str/replace code "pbkdf2+sha3_256" "") "$" "") 0 8)]
+        (twilio/with-auth PB_TWILIO_SERVICE_SID PB_TWILIO_AUTH_TOKEN
+          @(twilio/send-sms
+             {:From PB_TWILIO_PHONE_NUMBER
+              :To phone-number
+              :Body voting-code})))
       (let [code (hashers/derive phone-number {:alg :pbkdf2+sha3_256})
             voting-code (subs (str/replace (str/replace code "pbkdf2+sha3_256" "") "$" "") 0 8)]
         (db-tx db/create-voter! {:phone phone-number
                                  :admin false
                                  :is_active true
-                                 :code code})))))
-        ;return new code via twilio
+                                 :code code})
+        (twilio/with-auth PB_TWILIO_SERVICE_SID PB_TWILIO_AUTH_TOKEN
+          @(twilio/send-sms
+             {:From PB_TWILIO_PHONE_NUMBER
+              :To phone-number
+              :Body voting-code}))))))
 
 (defroutes api-routes
   (context "/api" []
