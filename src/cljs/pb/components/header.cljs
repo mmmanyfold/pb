8c2f7a1001904a88c02a2e79f4a89a91e2d2d4c0
David A. Viramontes <dviramontes@gmail.com>
2018-11-04 15:45:38 -0700

    feat: submit on enter key, refactor on clicks (#86)
    * feat: submit on enter key, refactor onclicks
    
    * fix: typo, security vulnerability in external link
    
    * fix: apply fix to submit code

--- src/cljs/pb/components/header.cljs
+++ src/cljs/pb/components/header.cljs
@@ -4,26 +4,26 @@
 
 (defn header-component [active-view election-slug count]
   (let [nav-item-classes "nav-item pl0 pr2 pr3-ns pb1"]
-    (fn [active-view election-slug count]
-      [:header {:class "fixed w-100 pt2 pt3-ns tracked bg-white"}
-       [rc/h-box
-        :class "nav-wrapper mh3 mh4-ns bb bw1 pb2 ttu"
-        :justify :between
-        :children [[:div {:class "flex"}
-                    [:a {:href "/#/"
-                         :class nav-item-classes}
-                     "PB Vote"]
-                    (when election-slug
-                      [:div {:class nav-item-classes} ">"])
-                    (when election-slug
-                      [:div {:class nav-item-classes} election-slug])]
-                   [:div {:class "nav-right flex items-center"}
-                    [:div {:class "subtitle f7 tr mr3"} "Participatory Budgeting"
-                     [:br] "in Denver, CO"]
-                    [:a {:href "http://thismachinehasasoul.com"
-                         :target "_blank"}
-                     [:img {:src "img/TMHAS_Logo_600.jpg"}]]]]]
-       (when (= active-view :proposals-view)
-         (when-let [{:keys [maxSelection]} @(rf/subscribe [:election-in-view])]
-           [:div.count-component.tc.mh3.mh4-ns.pa2
-            [:span (str count " / " maxSelection " selected")]]))])))
+    [:header {:class "fixed w-100 pt2 pt3-ns tracked bg-white"}
+     [rc/h-box
+      :class "nav-wrapper mh3 mh4-ns bb bw1 pb2 ttu"
+      :justify :between
+      :children [[:div {:class "flex"}
+                  [:a {:href "/#/"
+                       :class nav-item-classes}
+                   "PB Vote"]
+                  (when election-slug
+                    [:div {:class nav-item-classes} ">"])
+                  (when election-slug
+                    [:div {:class nav-item-classes} election-slug])]
+                 [:div {:class "nav-right flex items-center"}
+                  [:div {:class "subtitle f7 tr mr3"} "Participatory Budgeting"
+                   [:br] "in Denver, CO"]
+                  [:a {:href "http://thismachinehasasoul.com"
+                       :target "_blank"
+                       :rel "noopener noreferrer"}
+                   [:img {:src "img/TMHAS_Logo_600.jpg"}]]]]]
+     (when (= active-view :proposals-view)
+       (when-let [{:keys [maxSelection]} @(rf/subscribe [:election-in-view])]
+         [:div.count-component.tc.mh3.mh4-ns.pa2
+          [:span (str count " / " maxSelection " selected")]]))]))
--- src/cljs/pb/views/voting_code.cljs
+++ src/cljs/pb/views/voting_code.cljs
@@ -24,12 +24,12 @@
 
 (defn check-code [code election]
   (GET "/api/checkcode"
-       {:handler check-code-success-handler
-        :error-handler error-handler
+       {:handler         check-code-success-handler
+        :error-handler   error-handler
         :response-format (ajax/json-response-format {:keywords? true})
-        :format :raw
-        :params {:voter-code code
-                 :election election}}))
+        :format          :raw
+        :params          {:voter-code code
+                          :election   election}}))
 
 (defn send-code-success-handler []
   (reset! code-sent? true))
@@ -37,13 +37,13 @@
 
 (defn send-code [campus additional-id phone-number election]
   (POST "/api/votercode"
-       {:handler send-code-success-handler
-        :error-handler error-handler
-        :format (ajax/json-request-format)
-        :params {:campus campus
+        {:handler       send-code-success-handler
+         :error-handler error-handler
+         :format        (ajax/json-request-format)
+         :params        {:campus campus
                  :additional-id additional-id
-                 :election election
-                 :phone-number phone-number}}))
+                         :election      election
+                         :phone-number  phone-number}}))
 
 (defn error-component []
   (when-not (nil? @error-code)
@@ -71,28 +71,29 @@
     (reset! code-sent? false)
     (fn []
       (if-let [{additionalIdLabel :additionalIdLabel
-               startOnline :startOnline
-               endOnline :endOnline
-               {id :id} :sys} @(rf/subscribe [:election-in-view])]
+                startOnline       :startOnline
+                endOnline         :endOnline
+                {id :id}          :sys} @(rf/subscribe [:election-in-view])]
         (if (> (js/Date. endOnline) now)
           (if (> (js/Date. startOnline) now)
-           ;; if online voting hasn't started
-           [:div.voting-code-view
-            [:h1 (str "Voting opens on "
-                      (.format (js/moment startOnline) "dddd, MMMM Do YYYY [at] h:mm a."))]]
+            ;; if online voting hasn't started
+            [:div.voting-code-view
+             [:h1 (str "Voting opens on "
+                       (.format (js/moment startOnline) "dddd, MMMM Do YYYY [at] h:mm a."))]]
 
-           ;; if online voting is open
-           [:div.voting-code-view
-            (if (= @code-sent? false)
-              ;; if voting code hasn't been sent
-              [send-code-component additionalIdLabel id]
-              ;; if voting code has been sent
-              [check-code-component id])])
+            ;; if online voting is open
+            [:div.voting-code-view
+             (if (= @code-sent? false)
+               ;; if voting code hasn't been sent
+               [send-code-component additionalIdLabel id]
+               ;; if voting code has been sent
+               [check-code-component id])])
 
           ;; if online voting has ended
           [:div.voting-code-view
-          [:h1 (str "Voting ended on "
-                  (.format (js/moment endOnline) "dddd, MMMM Do YYYY [at] h:mm a."))]])
+           [:h1 (str "Voting ended on "
+                     (-> (js/moment endOnline)
+                         (.format "dddd, MMMM Do YYYY [at] h:mm a.")))]])
         [loading-component]))))
 
 
@@ -111,67 +112,63 @@
          [:div
           [:h1 "Create unique voting code"]
           [:form.voter-auth-form
-           (when-not (nil? additionalIdLabel)
+           {:on-submit (fn [e]
+                         (.preventDefault e)
+                         (.stopPropagation e))}
+           (when additionalIdLabel
              [:div
               [:div.flexrow.input-group-prepend
-               [:select {:id "campus" :class "form-control" :name "campus"
-                         :on-change (fn [e]
-                                      (let [input (-> e .-target .-value)]
-                                        (reset! campus input)))}
+               [:select {:id        "campus" :class "form-control" :name "campus"
+                         :on-change #(reset! campus (-> % .-target .-value))}
+
                 [:option {:default-value :disabled} "Campus:"]
                 [:option {:value "cudenver"} "CU Denver"]
                 [:option {:value "ccd"} "CCD"]
                 [:option {:value "msu"} "MSU"]]
                [:div.required "*"]
                [:input.form-control
-                {:type "text"
+                {:type        "text"
                  :placeholder "Student ID"
-                 :maxLength 10
-                 :value @additionalId
-                 :on-change (fn [e]
-                              (let [input (-> e .-target .-value)]
-                                (reset! additionalId input)))}]
+                 :maxLength   10
+                 :value       @additionalId
+                 :on-change   #(reset! additionalId (-> % .-target .-value))}]
                [:div.required "*"]]
               [:p [:small "Student IDs will be verified by each campus after the election, before the final vote count. Any votes associated with an invalid ID will not be counted."]]])
            [:div.flexrow.input-group-prepend
             [:input.form-control
-             {:id "input-phone1"
-              :type "text"
+             {:id          "input-phone1"
+              :type        "text"
               :placeholder "Enter Phone Number"
-              :maxLength 12
-              :value @input-phone1
-              :on-change (fn [e]
-                           (let [input (-> e .-target .-value)]
-                             (reset! input-phone1 input)))}]
+              :maxLength   12
+              :value       @input-phone1
+              :on-change   #(reset! input-phone1 (-> % .-target .-value))}]
             [:div.required "*"]]
 
            [:div.flexrow.input-group-prepend
             [:input.form-control
-             {:id "input-phone2"
-              :type "text"
+             {:id          "input-phone2"
+              :type        "text"
               :placeholder "Verify Phone Number"
-              :maxLength 12
-              :value @input-phone2
-              :on-change (fn [e]
-                           (let [input (-> e .-target .-value)]
-                             (reset! input-phone2 input)))}]
+              :maxLength   12
+              :value       @input-phone2
+              :on-change   #(reset! input-phone2 (-> % .-target .-value))}]
             [:div.required "*"]]
 
            [:h4 [:b "A text message with an 8-digit voting code will be sent to this phone number."]]
            [:p [:small "Your phone number will NEVER be shared and will be deleted automatically after this election."]]
            (when-not config/debug?
              [captcha-component])
-           [:a {:on-click #(send-code @campus @additionalId (string/replace @input-phone2 #" " "") id)}
-            [:input#send-code
-             {:type "button"
-              :value "SEND MY CODE"
+           [:a {:on-click #(send-code  @campus @additionalId (string/replace @input-phone2 #" " "") id)}
+            [:button#send-code
+             {:type     "submit"
               :disabled (or (< (count @input-phone2) 12)
                             (not= @input-phone1 @input-phone2)
                             (when-not config/debug?
                               (nil? @(rf/subscribe [:captcha-passed])))
                             (when-not (nil? additionalIdLabel)
                               (< (count @additionalId) 9)
-                              (= @campus "Campus:")))}]]]
+                              (= @campus "Campus:")))}
+             "SEND MY CODE"]]]
           [error-component]])})))
 
 (defn check-code-component [id]
@@ -182,18 +179,19 @@
        [:h2 "Check your text messages!"]
        [:h1 "Enter the 8-digit code:"]
        [:form.voter-auth-form
+        {:on-submit (fn [e]
+                      (.preventDefault e)
+                      (.stopPropagation e))}
         [:div.flex-row-wrap
          [:input.form-control
-          {:type "text"
+          {:type        "text"
            :placeholder "xxxxxxxx"
-           :maxLength 8
-           :value @code
-           :on-change (fn [e]
-                        (let [input (-> e .-target .-value)]
-                          (reset! code input)))}]
+           :maxLength   8
+           :value       @code
+           :on-change   #(reset! code (-> % .-target .-value))}]
          [:a {:on-click #(check-code @code id)}
-          [:input#submit-code
-           {:type "button"
-            :value "CONTINUE"
-            :disabled (< (count @code) 8)}]]]]
+          [:button#submit-code
+           {:type     "submit"
+            :disabled (< (count @code) 8)}
+           "CONTINUE"]]]]
        [error-component]])))
