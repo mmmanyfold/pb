70350b68d02f1a688551702dc1275e3b5786cddf
David A. Viramontes <dviramontes@gmail.com>
2018-11-25 12:14:29 -0700

    refactor: voting-code component

--- src/cljs/pb/views/voting_code.cljs
+++ src/cljs/pb/views/voting_code.cljs
@@ -36,15 +36,13 @@
   (reset! code-sent? true)
   (reset! error-code nil))
 
-(defn send-code [campus additional-id phone-number election]
+(defn send-code [phone-number election]
   (POST "/api/votercode"
         {:handler       send-code-success-handler
          :error-handler error-handler
          :format        (ajax/json-request-format)
-         :params        {:campus        campus
-                         :additional-id additional-id
-                         :election      election
-                         :phone-number  phone-number}}))
+         :params        {:election     election
+                         :phone-number phone-number}}))
 
 (defn error-component []
   (when-not (nil? @error-code)
@@ -54,15 +52,47 @@
       [:div.error.already-voted
        "We already got your vote!"])))
 
+
+(defn student-id-component [campus additionalId]
+  (let [empty-campus (rg/atom false)
+        empty-id (rg/atom false)]
+    [:div
+     [:div.flexrow.input-group-prepend
+      [:select {:id        "campus"
+                :class     (str "form-control" (when @empty-campus " input-error"))
+                :name      "campus"
+                :on-change (fn [e]
+                             (reset! campus (-> e .-target .-value))
+                             (reset! empty-campus (= (count @campus) 0)))}
+       [:option {:default-value :disabled
+                 :value         ""} "School:"]
+       [:option {:value "cudenver"} "CU Denver"]
+       [:option {:value "ccd"} "CCD"]
+       [:option {:value "msu"} "MSU"]]
+      [:div.required "*"]
+      [:input
+       {:type        "text"
+        :class       (str "form-control" (when @empty-id " input-error"))
+        :placeholder "xxxxxxxxx"
+        :maxLength   9
+        :value       @additionalId
+        :on-change   (fn [e]
+                       (reset! additionalId (-> e .-target .-value))
+                       (reset! empty-id (not= (count @additionalId) 9)))
+        :on-blur     (fn []
+                       (reset! empty-campus (= (count @campus) 0))
+                       (reset! empty-id (not= (count @additionalId) 9)))}]
+      [:div.required "*"]]]))
+
+
 (defn voting-code-view []
   (let [now (js/Date.)
         election-in-view (rf/subscribe [:election-in-view-2])]
     (if @election-in-view
       (let [_ (prn @election-in-view)
-            {additionalIdLabel :additionalIdLabel
-             startOnline       :startOnline
-             endOnline         :endOnline
-             {id :id}          :sys} @election-in-view]
+            {startOnline :startOnline
+             endOnline   :endOnline
+             {id :id}    :sys} @election-in-view]
         ;; TODO: undo these two conditionals
         (if true
           (if false
@@ -75,7 +105,7 @@
             [:div.voting-code-view
              (if (= @code-sent? false)
                ;; if voting code hasn't been sent
-               [send-code-component additionalIdLabel id]
+               [send-code-component @election-in-view]
                ;; if voting code has been sent
                [check-code-component id])])
 
@@ -87,12 +117,10 @@
       [loading-component])))
 
 
-(defn send-code-component [additionalIdLabel id]
+(defn send-code-component [election]
   (let [input-phone1 (rg/atom "")
         input-phone2 (rg/atom "")
         additionalId (rg/atom "")
-        empty-campus (rg/atom false)
-        empty-id (rg/atom false)
         empty-phone1 (rg/atom false)
         empty-phone2 (rg/atom false)
         phone-mismatch (rg/atom false)
@@ -103,95 +131,80 @@
          (new js/Cleave "#input-phone1" #js {:phone true :phoneRegionCode "US"})
          (new js/Cleave "#input-phone2" #js {:phone true :phoneRegionCode "US"}))
        :reagent-render
-       (fn []
-         [:div
-          [:h1 "Create unique voting code"]
-          [:form.voter-auth-form
-           {:on-submit (fn [e]
-                         (.preventDefault e)
-                         (.stopPropagation e))}
-           ;; TODO: refactor into own component
-           (when additionalIdLabel
-             [:div
-              [:div.flexrow.input-group-prepend
-               [:select {:id        "campus"
-                         :class     (str "form-control" (when @empty-campus " input-error"))
-                         :name      "campus"
-                         :on-change (fn [e]
-                                      (reset! campus (-> e .-target .-value))
-                                      (reset! empty-campus (= (count @campus) 0)))}
-                [:option {:default-value :disabled
-                          :value         ""} "School:"]
-                [:option {:value "cudenver"} "CU Denver"]
-                [:option {:value "ccd"} "CCD"]
-                [:option {:value "msu"} "MSU"]]
-               [:div.required "*"]
-               [:input
-                {:type        "text"
-                 :class       (str "form-control" (when @empty-id " input-error"))
-                 :placeholder "xxxxxxxxx"
-                 :maxLength   9
-                 :value       @additionalId
-                 :on-change   (fn [e]
-                                (reset! additionalId (-> e .-target .-value))
-                                (reset! empty-id (not= (count @additionalId) 9)))
-                 :on-blur     (fn []
-                                (reset! empty-campus (= (count @campus) 0))
-                                (reset! empty-id (not= (count @additionalId) 9)))}]
-               [:div.required "*"]]
-              [:p [:small "Student IDs will be verified by each campus after the election, before the final vote count. Any votes associated with an invalid ID will not be counted."]]])
-           [:div.flexrow.input-group-prepend
-            [:input
-             {:id          "input-phone1"
-              :type        "text"
-              :class       (str "form-control" (when (or @phone-mismatch
-                                                         @empty-phone1) " input-error"))
-              :placeholder "Enter Phone Number"
-              :maxLength   12
-              :value       @input-phone1
-              :on-change   (fn [e]
-                             (reset! input-phone1 (-> e .-target .-value))
-                             (when (and (= (count @input-phone1) 12)
-                                        (= (count @input-phone2) 12))
-                               (reset! phone-mismatch (not= @input-phone1 @input-phone2)))
-                             (reset! empty-phone1 (not= (count @input-phone1) 12)))
-              :on-blur     #(reset! empty-phone1 (not= (count @input-phone1) 12))}]
-            [:div.required "*"]]
+       (fn [election]
+         (let [{{additionalIdLabel    :additionalIdLabel
+                 phonePlaceholder1    :phonePlaceholder1
+                 phonePlaceholder2    :phonePlaceholder2
+                 votingCodeFieldLabel :votingCodeFieldLabel
+                 votingCodePageTitle  :votingCodePageTitle} :fields
+                {id :id}                                    :sys} election
+               is-auraria? (boolean additionalIdLabel)]
+           [:div
+            [:h1 votingCodePageTitle]
+            [:form.voter-auth-form
+             {:on-submit (fn [e]
+                           (.preventDefault e)
+                           (.stopPropagation e))}
+             (if is-auraria?
+               [student-id-component campus additionalId]
+               [:div
+                [:div.flexrow.input-group-prepend
+                 [:input
+                  {:id          "input-phone1"
+                   :type        "text"
+                   :class       (str "form-control" (when (or @phone-mismatch
+                                                              @empty-phone1) " input-error"))
+                   :placeholder phonePlaceholder1
+                   :maxLength   12
+                   :value       @input-phone1
+                   :on-change   (fn [e]
+                                  (reset! input-phone1 (-> e .-target .-value))
+                                  (when (and (= (count @input-phone1) 12)
+                                             (= (count @input-phone2) 12))
+                                    (reset! phone-mismatch (not= @input-phone1 @input-phone2)))
+                                  (reset! empty-phone1 (not= (count @input-phone1) 12)))
+                   :on-blur     #(reset! empty-phone1 (not= (count @input-phone1) 12))}]
+                 [:div.required "*"]]
 
-           [:div.flexrow.input-group-prepend
-            [:input
-             {:id          "input-phone2"
-              :type        "text"
-              :class       (str "form-control" (when (or @phone-mismatch
-                                                         @empty-phone2) " input-error"))
-              :placeholder "Verify Phone Number"
-              :maxLength   12
-              :value       @input-phone2
-              :on-change   (fn [e]
-                             (reset! input-phone2 (-> e .-target .-value))
-                             (when (and (= (count @input-phone1) 12)
-                                        (= (count @input-phone2) 12))
-                               (reset! phone-mismatch (not= @input-phone1 @input-phone2)))
-                             (reset! empty-phone2 (not= (count @input-phone2) 12)))
-              :on-blur     #(reset! empty-phone2 (not= (count @input-phone2) 12))}]
-            [:div.required "*"]]
+                [:div.flexrow.input-group-prepend
+                 [:input
+                  {:id          "input-phone2"
+                   :type        "text"
+                   :class       (str "form-control" (when (or @phone-mismatch
+                                                              @empty-phone2) " input-error"))
+                   :placeholder phonePlaceholder2
+                   :maxLength   12
+                   :value       @input-phone2
+                   :on-change   (fn [e]
+                                  (reset! input-phone2 (-> e .-target .-value))
+                                  (when (and (= (count @input-phone1) 12)
+                                             (= (count @input-phone2) 12))
+                                    (reset! phone-mismatch (not= @input-phone1 @input-phone2)))
+                                  (reset! empty-phone2 (not= (count @input-phone2) 12)))
+                   :on-blur     #(reset! empty-phone2 (not= (count @input-phone2) 12))}]
+                 [:div.required "*"]]
 
-           [:h4 [:b "A text message with an 8-digit voting code will be sent to this phone number."]]
-           [:p [:small "Your phone number will NEVER be shared and is automatically deleted after the election."]]
-           (when-not config/debug?
-             [captcha-component])
-           [:a {:on-click #(send-code @campus @additionalId (string/replace @input-phone2 #" " "") id)}
-            [:button#send-code
-             {:type     "submit"
-              :disabled (or (< (count @input-phone2) 12)
-                            (not= @input-phone1 @input-phone2)
-                            (when-not config/debug?
-                              (nil? @(rf/subscribe [:captcha-passed])))
-                            (when-not (nil? additionalIdLabel)
-                              (< (count @additionalId) 9)
-                              (= @campus "Campus:")))}
-             "CONTINUE"]]]
-          [error-component]])})))
+                [:h4 [:b "A text message with an 8-digit voting code will be sent to this phone number."]]])
+             [:p [:small votingCodeFieldLabel]]
+             (when-not config/debug?
+               [captcha-component])
+             [:a {:on-click #(if is-auraria?
+                               (prn "TODO: see auraria branch for how to handle student ID submit")
+                               (send-code (string/replace @input-phone2 #" " "") id))}
+              [:button#send-code
+               {:type     "submit"
+                :disabled (or (when-not config/debug?
+                                (nil? @(rf/subscribe [:captcha-passed])))
+                              (if is-auraria?
+                                (or (< (count @additionalId) 9)
+                                    (= @campus "Campus:"))
+                                (or (< (count @input-phone2) 12)
+                                    (not= @input-phone1 @input-phone2))))}
+               (if (= @(rf/subscribe [:language-in-view]) :en-US)
+                 "CONTINUE"
+                 "CONTINUAR")]]]
+            [error-component]]))})))
+
 
 (defn check-code-component [id]
   (let [code (rg/atom nil)]
